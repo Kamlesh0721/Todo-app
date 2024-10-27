7c2f86520fb336266cb52dc12c09379dcbb0e4ea
Kamlesh Singh <kamleshsingh0198@gmail.com>
2024-10-28 03:28:48 +0530

    add Heading and Add-feature-design

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,12 @@
+import Name from "./Components/Name";
+import Add from "./Components/Add";
 function App() {
-  return <center>ToDo App</center>;
+  return (
+    <center class="todo-container">
+      <Name />
+      <Add />
+    </center>
+  );
 }
 
 export default App;
---
+++ src/Components/Name.jsx
@@ -0,0 +1,4 @@
+function Name() {
+  return <h1>To Do App</h1>;
+}
+export default Name;
